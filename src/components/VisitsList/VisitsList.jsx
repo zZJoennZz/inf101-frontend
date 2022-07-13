90268f9e23d3122ff1888a2bfa79ea16b79bf61e
Joenn <zzjoennzz@gmail.com>
2022-07-14 03:20:07 +0800

    Added the fetch for the visits

--- src/components/VisitsList/VisitsList.jsx
+++ src/components/VisitsList/VisitsList.jsx
@@ -1,32 +1,49 @@
-import React from 'react';
-import './visitslist.scss';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./visitslist.scss";
 
 const VisitsList = (props) => {
+  const { data } = props;
 
-    const { data } = props;
-
-    return (
-        data.map(e => 
-            <div className="visits-list" key={e.visitId}>
-                <div className="visits-list-inner">
-                    <div className="row">
-                        <div className="col-2">
-                            <img src={e.userImg} alt={e.name} className="visits-list-client-img" />
-                        </div>
-                        <div className="col-8">
-                            <div className="visits-list-info"><span className="visits-list-label">Client:</span> <span className="visits-list-client-name">{e.name}</span> <span className="visits-list-client-id">{e.clientId}</span></div>
-                            <div className="visits-list-info"><span className="visits-list-label">Visit Date:</span> <span className="visits-list-date">{e.visitDate}</span></div>
-                            <button className="visits-list-view"><i className="fas fa-folder-open"></i> View</button>
-                        </div>
-                        <div className="col-2">
-                            <button className="visits-list-delete"><i className="fas fa-trash-alt"></i></button>
-                            <button className="visits-list-print"><i className="fas fa-print"></i></button>
-                        </div>
-                    </div>
-                </div>
+  return data.map((e) => (
+    <div className="visits-list" key={e.id}>
+      <div className="visits-list-inner">
+        <div className="row">
+          <div className="col-2">
+            <img
+              src={`${process.env.REACT_APP_IMG_URL}${e.image}`}
+              alt={e.name}
+              className="visits-list-client-img"
+            />
+          </div>
+          <div className="col-8">
+            <div className="visits-list-info">
+              <span className="visits-list-label">Client:</span>{" "}
+              <span className="visits-list-client-name">
+                {e.first_name} {e.middle_name} {e.last_name}
+              </span>{" "}
+              <span className="visits-list-client-id">{e.client_id}</span>
             </div>
-        )
-    )
-}
+            <div className="visits-list-info">
+              <span className="visits-list-label">Visit Date:</span>{" "}
+              <span className="visits-list-date">{e.visit_date}</span>
+            </div>
+            <button className="visits-list-view">
+              <Link to={"/visits/view/" + e.id}>
+                <i className="fas fa-folder-open"></i> View
+              </Link>
+            </button>
+          </div>
+          <div className="col-2">
+            <button className="visits-list-delete">
+              <i className="fas fa-trash-alt"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  ));
+};
 
 export default VisitsList;
