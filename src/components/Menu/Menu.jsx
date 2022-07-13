6e4525b66fb701b3cb3dc6634a0ddbfbcfcd5395
Joenn <zzjoennzz@gmail.com>
2022-07-14 03:19:20 +0800

    Made the code readable

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Menu = () => {
-    return (
-        <div className="menu">
-            <ul>
-                <li><NavLink to="/home" className={({ isActive }) => isActive ? "active" : ''}>Home</NavLink></li>
-                <li><NavLink to="/clients" className={({ isActive }) => isActive ? "active" : ''}>Clients</NavLink></li>
-                {/* <li><NavLink to="/visits" className={({ isActive }) => isActive ? "active" : ''}>Visits</NavLink></li> */}
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="menu">
+      <ul>
+        <li>
+          <NavLink
+            to="/home"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/clients"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Clients
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/visits"
+            className={({ isActive }) => (isActive ? "active" : "")}
+          >
+            Visits
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
